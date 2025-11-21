5e6ea316033a3eb256744a73eb992a01c9418bb1
Damien-16 <pv77ngi@eduvaud.ch>
2025-11-21 13:58:05 +0100

    feat (exercices) : space age

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -2,7 +2,26 @@
 // This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+const planetOrbitalPeriods = {
+    "mercury": 0.2408467,
+    "venus": 0.61519726,
+    "earth": 1.0,
+    "mars": 1.8808158,
+    "jupiter": 11.862615,
+    "saturn": 29.447498,
+    "uranus": 84.016846,
+    "neptune": 164.79132
 };
+
+export const age = (planet, secondes) => {
+    const orbitalPeriod = planetOrbitalPeriods[planet];
+
+    if (orbitalPeriod === undefined) {
+        throw new Error("not a planet");
+    }
+    const secondeAnnee = 31557600;  
+    const terreAnnee = secondes / secondeAnnee;
+    const ageSurPlanet = terreAnnee / orbitalPeriod;
+    return Math.round(ageSurPlanet * 100) / 100;
+};
+
