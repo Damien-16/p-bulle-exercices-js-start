c12cafc79c16d7ddd851d67328a7eaa8e2baadfa
Damien-16 <pv77ngi@eduvaud.ch>
2025-11-28 13:21:21 +0100

    feat (exercice) :fin ex gigasecond

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this line and implement the function');
+export const gigasecond = (dateDonnee) => {
+  return new Date(dateDonnee.getTime() + 1000000000000);
 };
