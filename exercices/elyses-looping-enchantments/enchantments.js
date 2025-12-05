642de5f9f9f9c8fe2b004db3bbf98c10b926c25b
Damien Chabal <pv77ngi@eduvaud.ch>
2025-12-05 16:06:04 +0100

    feat (exercie) fin elyses-looping-enchantement

--- exercices/elyses-looping-enchantments/enchantments.js
+++ exercices/elyses-looping-enchantments/enchantments.js
@@ -9,8 +9,13 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  // 🚨 Use .forEach
-  throw new Error('Implement the cardTypeCheck function');
+  let nbrCard = 0;
+  stack.forEach((c) => {
+    if (c === card) {
+      nbrCard++;
+    }
+  });
+  return nbrCard;
 }
 
 /**
@@ -21,6 +26,11 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  // 🚨 Use a `for...of` loop
-  throw new Error('Implement the determineOddEvenCards function');
+  let nbCard = 0;
+  for (const element of stack) {
+    if ((type && element % 2 === 0) || (!type && element % 2 !== 0)) {
+      nbCard++;
+    }
+  }
+  return nbCard;
 }
