8686d647f1c5db60663aad8ea11a4d850fec228f
Damien Chabal <pv77ngi@eduvaud.ch>
2025-12-19 14:27:48 +0100

    feat (exercices) : fin exo bird-watcher

--- exercices/bird-watcher/bird-watcher.js
+++ exercices/bird-watcher/bird-watcher.js
@@ -11,7 +11,11 @@
  * @returns {number} total bird count
  */
 export function totalBirdCount(birdsPerDay) {
-  throw new Error('Remove this line and implement the function');
+  let TotalBird = 0;
+  for (let i = 0; i < birdsPerDay.length; i++) {
+    TotalBird += birdsPerDay[i];
+  }
+  return TotalBird;
 }
 
 /**
@@ -22,7 +26,11 @@ export function totalBirdCount(birdsPerDay) {
  * @returns {number} birds counted in the given week
  */
 export function birdsInWeek(birdsPerDay, week) {
-  throw new Error('Remove this line and implement the function');
+  let TotalBird = 0;
+  for (let i = 0; i < 7; i++) {
+    TotalBird += birdsPerDay[i + (week-1) * 7];
+  }
+  return TotalBird;
 }
 
 /**
@@ -33,5 +41,9 @@ export function birdsInWeek(birdsPerDay, week) {
  * @returns {void} should not return anything
  */
 export function fixBirdCountLog(birdsPerDay) {
-  throw new Error('Remove this line and implement the function');
+for (let i = 0;i < birdsPerDay.length; i++){
+  if (i % 2 == 0 ){
+    birdsPerDay[i]+=1;
+  }
+}
 }
