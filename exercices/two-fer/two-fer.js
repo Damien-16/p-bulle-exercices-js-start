6d02050b1e35304c73305226e9205cc782fd87cf
Damien Chabal <pv77ngi@eduvaud.ch>
2026-01-09 13:16:49 +0100

    fix(exercices) : two-fer

--- exercices/two-fer/two-fer.js
+++ exercices/two-fer/two-fer.js
@@ -3,7 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = (name = "you") => {
-  return `One for ${name}, one for me.`;
+export const twoFer = (name) => {
+   const recipient = name || "you";
+  return "One for "+recipient+", one for me.";
 
 };
